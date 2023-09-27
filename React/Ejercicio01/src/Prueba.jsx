aabf31d71427ba192404c12bb304598be425fe28
“Mrcx3” <marcelo.m95@hotmail.com>
2023-09-26 22:05:47 -0300

    I

---
+++ React/Ejercicio01/src/Prueba.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Prueba = () => {
+    const titulo="Hello dog";
+    const subTitulo="Sub titulo";
+  return (
+    <div>
+    <h1>{titulo}</h1>
+    <h2>{subTitulo}</h2>
+    </div>
+  );
+};
