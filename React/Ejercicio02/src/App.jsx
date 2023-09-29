f3a989740aacec520af20dde014b7fc158d8e413
“Mrcx3” <marcelo.m95@hotmail.com>
2023-09-29 19:43:42 -0300

    i

---
+++ React/Ejercicio02/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import Navbar from './Navbar';
+import Main from './main';
+import Footer from './footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
