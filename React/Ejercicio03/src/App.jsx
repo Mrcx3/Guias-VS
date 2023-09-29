aed56a12f6040bb33517b998ff9c66ecbcf09875
“Mrcx3” <marcelo.m95@hotmail.com>
2023-09-29 20:18:14 -0300

    i

---
+++ React/Ejercicio03/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Main from './main'
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Ejercicio03/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Hijo from './Hijo.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+const Main = () => {
+  //dentro de un div paso dos instancias de Hijo  a cada Hijo se le pasa una prop
+  //llamada nombre que contiene lo que hay entre comillas
+  return (<div><Hijo nombre="Chiquito"/> 
+  <Hijo nombre="Filomena" /></div>
+  );
+};
+export default Main;
