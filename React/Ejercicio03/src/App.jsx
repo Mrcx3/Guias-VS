6318bbbdec0ac3f5f36dc1dbee73c0ff3534cbc3
“Mrcx3” <marcelo.m95@hotmail.com>
2023-10-02 16:33:18 -0300

    i

--- React/Ejercicio03/src/App.jsx
+++ React/Ejercicio03/src/App.jsx
@@ -13,3 +13,12 @@ function App() {
 }
 
 export default App;
+// Crear un Componente Main el cual llame dos veces a un mismo componente, es decir, 
+// que Main anide a Hijo e Hijo. 
+// • main.jsx
+// o App
+// § Main
+// • Hijo
+// • Hijo 
+// Al primer Componente anidado pasarle como props el nombre Chiquito y al segundo el 
+// nombre Filomena. Desde los componentes Hijos atrapar los valores mediante las props.
--- React/Ejercicio03/src/Hijo.jsx
+++ React/Ejercicio03/src/Hijo.jsx
@@ -1,4 +1,5 @@
 import React from "react";
-// Se crea el componente Hijo, que retorna  h1 y muestra un mensaje con el valor de props.nombre
+// Se crea el componente Hijo, que contiene propiedades y retorna h1 
+// y muestra un mensaje con el valor de props.nombre
 const Hijo = (props) => { return <h1>El nombre guardado es {props.nombre}</h1>; };
 export default Hijo;
