c848c5f199aa6d68a6bc0341dc8d9eef778ad83f
“Mrcx3” <marcelo.m95@hotmail.com>
2023-10-09 18:50:27 -0300

    i

---
+++ React/Ejercicio05/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'; // Importar React y el hook useState desde React
+
+function Contador() { 
+  const [contador, setContador] = useState(0); 
+  // hook useState para crear variable de estado contador 
+  //y una función para actualizarla llamada setContador. El estado inicial es 0.
+
+  return ( 
+    <div>
+      <p>Has hecho clic {contador} veces</p> 
+      <button onClick={() => setContador(contador + 1)}> // Botón que, cuando se hace clic en él, incrementa contador en 1
+        Haz clic aquí
+      </button>
+    </div>
+  );
+}
+
+export default Contador;
